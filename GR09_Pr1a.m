58b003d805b874c0736eb35590f2e7d0802af767
gadria17 <gilia061723@gmail.com>
2026-09-11 16:40:48 +0200

    Hechos ejercicios del 3 al 8, además de eliminar los pause para más comodidad.

--- GR09_Pr1a.m
+++ GR09_Pr1a.m
@@ -1,3 +1,5 @@
+repo=gitrepo
+
 %%1. Cree un vector fila, de nombre u, con componentes 1, 3, 5, y otro vector 
 %fila v, con componentes 2, 4, 6. Calcule el producto escalar de ambos 
 % vectores de dos formas, usando y sin usar el mandato dot.
@@ -34,7 +36,58 @@ A=[1,2,3;
 
 I= eye(3,3)
 
+A_tras=A'
 
+B=A'*I
+
+%%4.Usando la notación ":" cree ahora un vector columna, de nombre AC2, con 
+% el contenido de la segunda columna de la matriz A, y un vector fila, de 
+% nombre AF3, con el contenido de la tercera fila de la matriz A.
+
+AC2=A(:,2)
+AF3=A(3,:)
+
+%%5.Usando el operador .^, cree una matriz cuyos elementos sean, uno a uno, 
+% los de la matriz A elevados al cuadrado. Compare este resultado con el de
+% la operación A.*A (y verá que son iguales) y con el del producto 
+% matricial A*A (y verá que son distintos). Este último, sin embargo, 
+% coincidirá con el de A^2.
+
+A_elevado=A.^2
+A_elevado2=A.*A
+A_matricial=A*A
+A_3=A^2
+
+%%6. Extraiga la componente (2,3) (fila 2, columna 3) de la matriz A y 
+% multiplíquela por un vector que tenga por componentes los elementos de la
+% diagonal principal de la matriz B (mandato diag). Llame P al resultado. 
+% Fíjese en si éste es un vector fila o columna. Calcule las dimensiones 
+% (longitud) del vector P (mandato length).
+
+A23=A(2,3)
+Bdiag=diag(B)
+P=A23*Bdiag
+%P es un vector columna y por tanto su dimensión es 3
+dim_P=length(P)
+
+%%7. Cree una matriz 2x3, de nombre C, cuyos componentes sean todos 1, y 
+%multiplíquela por una matriz 3x3, de nombre Aleat, con componentes 
+%escogidos aleatoriamente por Matlab. Llame E a la matriz resultante. 
+%Fíjese en si los elementos de Aleat tienen todos el mismo signo y entre 
+%qué valores varían. Calcule las dimensiones de la matriz E (mandato size).
+
+C=ones(2,3)
+Aleat=rand(3,3)
+E=C*Aleat
+dimensiones_E=size(E)
+%Los valores de los elementos de Aleat varían entre 0 y 1, todos ellos
+%positivos
+
+%%8. Usando el mandato linspace, cree un vector, de nombre U, con 11 
+% elementos (incluyendo los extremos) equiespaciados entre 0 y 1. Realice 
+% la misma operación (llame ahora V al vector) usando la notación ":".
+U=linspace(0,1,11)
+V=[0:0.1:1]
 
 
 
