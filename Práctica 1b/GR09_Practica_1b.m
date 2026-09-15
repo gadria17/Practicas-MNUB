5194292ab65c2de44ca8d19cc480de71a61e8e85
gadria17 <gilia061723@gmail.com>
2026-09-15 13:38:44 +0200

    Ejercicios hechos hasta 12.

--- Práctica 1b/GR09_Practica_1b.m
+++ Práctica 1b/GR09_Practica_1b.m
@@ -23,7 +23,40 @@ datosY=matrizDatos(:,2);
 % valores de x dados por datosX y llame vectorF1 al vector resultante de 
 % esta evaluación.
 
-vectorF1=GR09_funcRR_Practica1b(datosX,)
+vectorF1=GR09_funcRR_Practica1b(datosX);
+
+%%9. Usando los mandatos max, min, mean y std calcule los valores máximo, 
+% mínimo, medio y desviación estándar, respectivamente, de vectorF1, 
+% llamando a los resultados correspondientes maximoF1, minimoF1, mediaF1, y
+% desvestF1 (NOTA: no debe confundirse el mandato mean, que calcula la 
+% media de una serie de valores, con el mandato median, que calcula su 
+% mediana). Deje que se muestren por pantalla los resultados.
+
+maximoF1=max(vectorF1);
+minimoF1=min(vectorF1);
+mediaF1=mean(vectorF1);
+desvestF1=std(vectorF1);
+
+%%10. Usando el mandato fprintf, haga que se muestre por pantalla el 
+% mensaje "El valor medio de vectorF1 es", seguido del valor de la variable
+% mediaF1. Introduzca un salto de línea.
+
+fprintf("El valor medio de vectorF1 es\n" + mediaF1);
+
+%%11. Evalúe ahora la función definida en GRxx_R2R_Practica1b.m para los 
+% valores de x e y dados por datosX y datosY y llame vectorF2 al vector 
+% resultante de esta evaluación.
+
+vectorF2=GR09_funcR2R_Practica1b(datosX,datosY);
+
+%%12. Evalúe a continuación la función definida en GRxx_R2R2_Practica1b.m 
+% para los valores de x e y dados por datosX y datosY y llame vectorF3 y 
+% vectorF4 a los vectores resultantes de esta evaluación.
+
+vectorF3=GR09_funcR2R2_Practica1b(datosX,datosY);
+vectorF4=GR09_funcR2R2_Practica1b(datosX,datosY);
+
+
 
 
 
--- Práctica 1b/GR09_funcRR_Practica1b.m
+++ Práctica 1b/GR09_funcRR_Practica1b.m
@@ -1,21 +1,6 @@
-function [y] = RR(x);
+function [y] = GR09_funcRR_Practica1b(x);
 
-y= 3*x.^2 + 4*e.^x + cos(x)
+y=3*x.^2 + 4*exp(x) + cos(x);
 
 end
 
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-arguments (Input)
-    inputArg1
-    inputArg2
-end
-
-arguments (Output)
-    outputArg1
-    outputArg2
-end
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
